17bbfd73284a31b84e63449111f8fe2fb38375d3
TrubachNikita <kakpuckeececes@gmail.com>
2026-06-03 19:46:11 +0300

    chore: очистка ветки review для ревью кода

--- frontend/src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-
-declare global {
-  interface Window {
-    addToCart: (productId: string) => Promise<void>;
-    updateCartItem: (productId: string, action: 'add' | 'subtract' | 'remove') => Promise<void>;
-    showToast: (message: string, isError?: boolean) => void;
-  }
-}
